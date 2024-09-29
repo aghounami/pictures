ab688643c13280160f2f9ebfb086d7a0b188e91c
Ahmed Ghounami <aghounam@e3r3p7.1337.ma>
2024-09-29 15:35:34 +0100

    pictures

--- index.js
+++ index.js
@@ -3,18 +3,40 @@ const displayedImage = document.querySelector('.displayed-image');
 const button_left = document.querySelector('.prev');
 const button_right = document.querySelector('.next');
 
+update = () => {
+    for(let i = 0; i < images.length; i++) {
+        if (images[i].classList.contains('selected')) {
+            displayedImage.src = images[i].src;
+        }
+    }
+}
+
+make_in_center = (c) => {
+    for(let i = 0; i < images.length; i++) {
+        var tmp = images[i].src;
+        if (i + c < 0)
+        {
+            images[i].src = images[images.length - 1].src;
+            images[images.length - 1].src = tmp;
+        }
+        else if (i + c >= images.length)
+        {
+            images[i].src = images[0].src;
+            images[0].src = tmp;
+        }
+        else
+        {
+            images[i].src = images[i + c].src;
+            images[i + c].src = tmp;
+        }
+    }
+}
+
 prevImage = () => {
     for (let i = 0; i < images.length; i++) {
-        if (images[i].classList.contains('selected')) {
-            images[i].classList.remove('selected');
-            if (i === 0) {
-                i = images.length;
-            }
-            images[i - 1].classList.add('selected');
-            displayedImage.src = images[i - 1].src;
-            
-            break;
-        }
+            let c = -1;
+            make_in_center(-1);
+            update();
     }
 }
 
@@ -24,20 +46,21 @@ button_left.addEventListener('click', prevImage);
 
 nextImage = () => {
     for (let i = 0; i < images.length; i++) {
-        if (images[i].classList.contains('selected')) {
-            images[i].classList.remove('selected');
-            if (i === images.length - 1) {
-                i = -1;
-            }
-            images[i + 1].classList.add('selected');
-            displayedImage.src = images[i + 1].src;
-            
-            break;
-        }
+            let c = 1;
+            make_in_center(c);
+            update();
     }
 }
 
 button_right.addEventListener('click', nextImage);
 
-
-
+for(let i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', () => {
+        for (let j = 0; j < images.length; j++) {
+            images[j].classList.remove('selected');
+        }
+        images[i].classList.add('selected');
+        displayedImage.src = images[i].src;
+    }
+    );
+}
